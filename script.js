ab8978851d37986e3153546561bad06417627fa3
Nizamuddin <nizamuddinshaik997@gmail.com>
2025-08-01 18:38:02 +0530

    add function to the buttons and recently clicked button function

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let gridBox;
 for(let i = 1;i <= pixels**2;i++){
     gridBox = document.createElement("div");
     gridBox.classList.add('grid-box');
+    gridBox.style.backgroundColor = "white";
     gridBox.style.height = pixelsize + "px";
     gridBox.style.width = pixelsize + "px";
     grid.appendChild(gridBox);
@@ -12,5 +13,53 @@ for(let i = 1;i <= pixels**2;i++){
 
 grid.addEventListener('mouseover', (e) => {
     let target = e.target;
-    target.style.backgroundColor = "black";
+    if (target.id === 'grid'){
+        target.style.backgroundColor = 'white';
+    }
+    else{
+        target.style.backgroundColor = 'black';
+    }
 })
+
+let selectedButton = document.querySelector('.selected-button');
+let controls = document.querySelector('.controls');
+controls.addEventListener('click', (e) =>{
+    let target = e.target;
+    let button = target.id;
+    selectedButton.textContent = button;
+    switch(button){
+        case "edit":
+           pixels = prompt("How many pixles do you want?(1 - 100)");
+           grid.replaceChildren();
+           pixelsize = 300/pixels;
+           for(let i = 1;i <= pixels**2;i++){
+                gridBox = document.createElement("div");
+                gridBox.classList.add('grid-box');
+                gridBox.style.backgroundColor = "white";
+                gridBox.style.height = pixelsize + "px";
+                gridBox.style.width = pixelsize + "px";
+                grid.appendChild(gridBox);
+            }
+           break;
+        case "color":
+            grid.addEventListener('mouseover', (e) => {
+                let target = e.target;
+                target.style.backgroundColor = "black";
+                })
+            break;
+        case "clear":
+            let gridBoxs = grid.querySelectorAll('.grid-box');
+            gridBoxs.forEach((box) => {
+                box.style.backgroundColor = 'white';
+            })
+            break;
+        case "eraser":
+            grid.addEventListener('mouseover', (e) => {
+                let target = e.target;
+                target.style.backgroundColor = "white";
+                })
+            break;
+
+    } 
+})
+
